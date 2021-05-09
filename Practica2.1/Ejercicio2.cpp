0386f994685298ded89c9f605e5880bb890947fd
Eloy Moreno Cortijo <eloymore@ucm.es>
2021-05-09 19:41:40 +0200

    Hecho el ejercicio 2

---
+++ Practica2.1/Ejercicio2.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char** argv){
+    if(argc < 3 || argc > 3){
+        std::cout << "Uso: ./ejercicio2 <ipv4> <port>" << std::endl;
+        return -1;
+    }
+
+    struct addrinfo* info;
+    struct addrinfo hints;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+
+    int err = getaddrinfo(argv[1], argv[2], &hints, &info);
+
+    if(err != 0){
+        std::cerr << "Error getaddrinfo: " << gai_strerror(err) << std::endl;
+        return err;
+    }
+
+    int sock = socket(info->ai_family, info->ai_socktype, 0);
+    if(sock < 0){
+        std::cerr << "Error socket: " << strerror(errno) << std::endl; 
+        return -errno;
+    }
+
+    err = bind(sock, info->ai_addr, info->ai_addrlen);
+    if(err < 0){
+        std::cerr << "Error bind: " << strerror(errno) << std::endl; 
+        return -errno;
+    }
+
+    freeaddrinfo(info);
+
+    char buffer[2];
+    memset(&buffer, 0, 2);
+
+    struct sockaddr cliente;
+    socklen_t clienteLength = sizeof(struct sockaddr);
+    while(buffer[0] != 'q'){
+
+        int bread = recvfrom(sock, (void*)buffer, 2, 0, &cliente, &clienteLength);
+        if(bread < 0){
+            std::cerr << "Error recvfrom: " << strerror(errno) << std::endl; 
+            return -errno;
+        }
+
+        char host[NI_MAXHOST];
+        char serv[NI_MAXSERV];
+
+        int err2 = getnameinfo(&cliente, clienteLength, host, NI_MAXHOST, serv, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
+        if(err2 != 0){
+            std::cerr << "Error getnameinfo: " << gai_strerror(err2) << std::endl;
+            return err2;
+        }
+
+        std::cout << bread << " bytes de " << host << ":" << serv << std::endl;
+
+        char sendToBuffer[256];
+        size_t sendToSize = 0;
+        switch (*buffer)
+        {
+        case 't':{
+            time_t rawt = time(nullptr);
+            struct tm* t;
+            t = localtime(&rawt);
+            sendToSize = strftime(sendToBuffer, 256, "%X %p", t);
+            break;
+            }
+        case 'd':{
+            time_t rawt = time(nullptr);
+            struct tm* t;
+            t = localtime(&rawt);
+            sendToSize = strftime(sendToBuffer, 256, "%y-%m-%d", t);
+            break;
+            }
+        case 'q':{
+            std::cout << "Saliendo...\n";
+            break;
+        }
+        default:{
+            std::cerr << "Comando no soportado " << *buffer << std::endl;
+            break;
+            }
+        }
+
+        if(sendToSize > 0){
+            sendToBuffer[sendToSize] = '\0';
+            int err = sendto(sock, sendToBuffer, sendToSize, 0, &cliente, clienteLength);
+            if(err < 0){
+                std::cerr << "Error sendto: " << strerror(errno) << std::endl; 
+                return -errno;
+            }
+        }
+    }
+
+    err = close(sock);
+    if(err < 0){
+        std::cerr << "Error close: " << strerror(errno) << std::endl; 
+        return -errno;
+    }
+
+    return 0;
+}
