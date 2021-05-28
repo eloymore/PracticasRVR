3692462ec80d42cf807c30290966b601421b7ed6
Eloy Moreno Cortijo <eloymore@ucm.es>
2021-05-28 12:26:15 +0200

    Hecho el ejercicio 2

---
+++ Practica2.2/Ejercicio 2.cc
@@ -0,0 +1,89 @@
+#include "Serializable.h"
+
+#include <iostream>
+#include <string>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+
+class Jugador: public Serializable
+{
+public:
+    Jugador(const char * _n, int16_t _x, int16_t _y):x(_x),y(_y)
+    {
+        strncpy(name, _n, MAX_NAME);
+    };
+
+    virtual ~Jugador(){};
+
+    void to_bin()
+    {
+        int32_t data_size = 80 + 2 * sizeof(int16_t);
+        alloc_data(data_size);
+
+        char * bin = _data;
+
+        memcpy(bin, name, MAX_NAME);
+        bin += MAX_NAME;
+
+        memcpy(bin, &x, sizeof(int16_t));
+        bin += sizeof(int16_t);
+
+        memcpy(bin, &y, sizeof(int16_t));
+        bin += sizeof(int16_t);
+    }
+
+    int from_bin(char * data)
+    {
+        char * bin = _data;
+
+        memcpy(name, bin, MAX_NAME);
+        bin += MAX_NAME;
+
+        memcpy(&x, bin, sizeof(int16_t));
+        bin += sizeof(int16_t);
+
+        memcpy(&y, bin, sizeof(int16_t));
+        bin += sizeof(int16_t);
+        return 0;
+    }
+
+
+public:
+    static const size_t MAX_NAME = 80;
+
+    char name[MAX_NAME];
+
+    int16_t x;
+    int16_t y;
+};
+
+int main(int argc, char **argv)
+{
+    Jugador one_w("Player_ONE", 123, 987);
+
+    // Serializar el objeto one_w
+    one_w.to_bin();
+    // Escribir la serialización en un fichero
+    int fd = open("./data_jugador", O_CREAT | O_TRUNC | O_RDWR, 0666);
+    if (fd == -1){
+        perror("No se puede crear el archivo ./data_jugador");
+        return -1;
+    }
+    if(write(fd, one_w.data(), one_w.size()) == -1){
+        perror("No se ha podido escribir en el archivo");
+        return -errno;
+    }
+    // Cerrar el archivo
+    if(close(fd) == -1){
+        perror("No se ha podido cerrar el archivo");
+        return -errno;
+    }
+
+    return 0;
+}
+
